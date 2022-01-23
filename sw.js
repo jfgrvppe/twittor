2fb4c8c1f2797eda0459520cc11d6e07e1cdd40b
Jordi Fernandez <jordi.fernandez@grvppe.com>
2022-01-23 09:59:38 +0100

    Modificaciones para IOS

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //IMPORTS
 importScripts("js/sw-utils.js");
 
-const STATIC_CACHE = "static_v2";
+const STATIC_CACHE = "static_v3";
 const STATIC_DYNAMIC = "dynamic_v1";
 const INMUTABLE_CACHE = "inmutable_v1";
 
